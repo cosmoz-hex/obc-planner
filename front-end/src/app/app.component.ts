739dbecb38094a684b1abb877c2325cebd94e5fb
Alan Mocaer <alanmoc@hotmail.fr>
2026-06-28 15:12:59 +0200

    Update Angular to v19

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -4,11 +4,10 @@ import { TestService } from './services/test.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'front-end';
